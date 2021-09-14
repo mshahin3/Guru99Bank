509bb87fc5ae9961f7d5129d7ec308ee97f71895
mshahin3@yahoo.com <mshahin3@yahoo.com>
2021-09-14 10:05:57 -0400

    Add constructor to EditcustomerPage

--- src/main/java/com/guru99/pages/EditCustomerPage.java
+++ src/main/java/com/guru99/pages/EditCustomerPage.java
@@ -1,7 +1,17 @@
 package com.guru99.pages;
 
+import org.openqa.selenium.WebDriver;
+
 import com.guru99.base.BasePage;
 
 public class EditCustomerPage extends BasePage{
+	public WebDriver driver;
+	
+	public EditCustomerPage(WebDriver driver) {
+		this.driver =driver;
+	}
+	
+	
+	
 
 }
