5056a3f31ae30377bd02f986475ce0855d1541f8
mshahin3@yahoo.com <mshahin3@yahoo.com>
2021-09-15 09:35:18 -0400

    added deleteCustomerPage

---
+++ src/main/java/com/guru99/pages/DeleteCustomerPage.java
@@ -0,0 +1,18 @@
+package com.guru99.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.guru99.base.BasePage;
+
+public class DeleteCustomerPage extends BasePage {
+	
+	public WebDriver driver;
+	
+	public DeleteCustomerPage(WebDriver driver) {
+		
+		this.driver=driver;
+		
+	}
+	
+
+}
