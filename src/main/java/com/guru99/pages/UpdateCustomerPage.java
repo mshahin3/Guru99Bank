e8c7e2f48a98bdbe7c6e98c535844b64cfb13ff5
mshahin3@yahoo.com <mshahin3@yahoo.com>
2021-09-15 10:18:38 -0400

    added to dev branch

---
+++ src/main/java/com/guru99/pages/UpdateCustomerPage.java
@@ -0,0 +1,16 @@
+package com.guru99.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.guru99.base.BasePage;
+
+public class UpdateCustomerPage extends BasePage {
+	
+	
+	WebDriver driver;
+	
+	public UpdateCustomerPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+}
